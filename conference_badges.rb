22af19b44d69eb61e77226400bdaa1cbaed30c1f
Sara A <calisaraalhindi@gmail.com>
2020-08-06 00:25:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,9 +22,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |a|
-    return a
+    puts a
   end
   assign_rooms(attendees).each do |b|
-   return b
+   puts b
   end
 end
